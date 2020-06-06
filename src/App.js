39fc6c54ebe83fdcd1a0554d8f8ed14cddca4e55
doduythinh1 <doduythinh1@gmail.com>
2020-06-06 16:06:33 +0700

    https://github.com/doduythinh/ReactJS/issues/11

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./Containers/CheckOut/Checkout";
 
 import { Route, Switch } from 'react-router-dom'
-
+import Orders from "./Containers/Orders/Orders";
 class App extends Component{
   render() {
     return (
@@ -16,6 +16,7 @@ class App extends Component{
             <Layout>
                 <Switch>
                     <Route path="/checkout" component={Checkout} />
+                    <Route path="/orders" component={Orders} />
                     <Route path="/" exact component={BurgerBuilder} />
                 </Switch>
             </Layout>
