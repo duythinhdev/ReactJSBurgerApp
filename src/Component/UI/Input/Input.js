842ac93a9f7049049199b97fe2c0c7e67a50022c
doduythinh1 <doduythinh1@gmail.com>
2020-06-06 17:43:46 +0700

    https://github.com/doduythinh/ReactJS/issues/13

--- src/Component/UI/Input/Input.js
+++ src/Component/UI/Input/Input.js
@@ -1,12 +1,26 @@
 
 import React from "react";
+import styles from './Input.css';
 
 const  input = (props) => {
 
-    
+    let  inputElement = null;
+
+    switch (props.inputtype) {
+        case ('input'):
+            inputElement = <input className="InputElement" {...props} />;
+            break;
+        case ('textarea'):
+            inputElement = <textarea className="InputElement" {...props} />;
+            break;
+        default:
+                inputElement = <input className="InputElement" {...props} />;
+
+    }
     return (
-        <div>
-            <label>{props.label}</label>
+        <div className="Input">
+            <label className="label">{props.label}</label>
+            {inputElement}
         </div>
     );
 };
