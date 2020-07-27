1009104e9802e64e0d7c01375842c7817b270e6d
doduythinh1 <doduythinh1@gmail.com>
2020-07-27 23:22:00 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import App1 from'./App.css';
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder";
